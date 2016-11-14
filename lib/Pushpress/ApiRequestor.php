d9f07e08611065554e5316bb194e74a02f517129
Patrick Santora <pat@djcity.com>
2016-11-14 13:26:22 -0800

    no message

--- lib/Pushpress/ApiRequestor.php
+++ lib/Pushpress/ApiRequestor.php
@@ -129,8 +129,8 @@ class Pushpress_ApiRequestor
     if (PushpressApi::$apiVersion)
       $headers[] = 'Pushpress-Version: ' . PushpressApi::$apiVersion;
 
-    if ($_SERVER['X-Forwarded-For'])
-      $headers[] = 'X-Forwarded-For: ' . $_SERVER['X-Forwarded-For'];
+    if ($_SERVER['HTTP_X_FORWARDED_FOR'])
+      $headers[] = 'X-Forwarded-For: ' . $_SERVER['HTTP_X_FORWARDED_FOR'];
 
     list($rbody, $rcode) = $this->_curlRequest($meth, $absUrl, $headers, $params);
     return array($rbody, $rcode, $myApiKey);
