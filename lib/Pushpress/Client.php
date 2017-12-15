4c3431d463e2ff8d33f755a68b19dcad86e3f64e
Dan Uyemura <dan@pushpress.com>
2017-12-14 16:53:29 -0800

    Added client settings save function to Client object.

--- lib/Pushpress/Client.php
+++ lib/Pushpress/Client.php
@@ -45,14 +45,39 @@ class Pushpress_Client extends Pushpress_ApiResource
     return self::_scopedRetrieveEmpty($class, $id, $apiKey);
   }
   
-    public static function settings($type=null) {
+  public static function settings($type=null) {
         $class = get_class();
         $url = self::classUrl($class);
         $url .= "/settings/$type";
         $requestor = new Pushpress_ApiRequestor();
         list($response, $apiKey) = $requestor->request('get', $url);
         return Pushpress_Util::convertToPushpressObject($response, $apiKey); 
-    }
+  }
+
+  public static function saveSettings($type=null, $settings=array()) {
+        $class = get_class();
+        $url = self::classUrl($class);
+        $url .= "/settings/";
+        $requestor = new Pushpress_ApiRequestor();
+
+        if (is_array($type)) { 
+          // batch setting to save
+          $params = array(
+            "batch" => json_encode($type)
+          );
+        }
+        else { 
+          $params = array(
+            "batch" => array(
+              "type" => $type,
+              "batch" => json_encode($settings)
+            )          
+          );  
+        } 
+
+        list($response, $apiKey) = $requestor->request('post', $url, $params);
+        return Pushpress_Util::convertToPushpressObject($response, $apiKey); 
+  }
     
     public static function referralSources() {
         $class = get_class();
